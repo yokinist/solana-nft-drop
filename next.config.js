343618263ad374e08d11341a150336f68c8593c6
yokinist <ikd618@gmail.com>
2022-03-21 22:51:18 +0900

    chore: set Environment Variables

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
+const { CANDY_MACHINE_ID, SOLANA_NETWORK, SOLANA_RPC_HOST } = process.env;
+
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  env: {
+    CANDY_MACHINE_ID,
+    SOLANA_NETWORK,
+    SOLANA_RPC_HOST,
+  },
 };
