00c0cfdb1f167be0fb14828509b8a475060f079f
yokinist <ikd618@gmail.com>
2022-03-24 08:39:39 +0900

    fix: formatDate

--- src/libs/date.ts
+++ src/libs/date.ts
@@ -1,7 +1,13 @@
 import { format, formatDistanceToNow, parseISO } from 'date-fns';
 import { ja } from 'date-fns/locale';
 
-export const formatDate = (dateString?: string) => {
+export const formatDate = (date?: Date): string => {
+  if (!date) return '';
+  const formattedDate = format(date, 'yyyy/MM/dd - hh:mm', { locale: ja });
+  return formattedDate;
+};
+
+export const formatDateString = (dateString?: string) => {
   if (!dateString) return '';
   const date = parseISO(dateString);
   return format(date, 'yyyy/MM/dd', { locale: ja });
